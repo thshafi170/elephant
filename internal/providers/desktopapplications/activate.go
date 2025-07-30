603f26083c6e65090b84d758b46d3cf0a6b72943
Andrej Benz <hello@benz.dev>
2025-07-30 14:45:45 +0200

    desktopapplications: trim action from identifier without launch prefix

--- internal/providers/desktopapplications/activate.go
+++ internal/providers/desktopapplications/activate.go
@@ -3,11 +3,16 @@ package main
 import (
 	"log/slog"
 	"os/exec"
+	"strings"
 
 	"github.com/abenz1267/elephant/internal/common"
 )
 
 func Activate(qid uint32, identifier, action string) {
+	if config.LaunchPrefix == "" {
+		identifier = strings.Split(identifier, ":")[0]
+	}
+
 	cmd := exec.Command("sh", "-c", common.WrapWithPrefix(config.LaunchPrefix, identifier))
 
 	err := cmd.Start()
